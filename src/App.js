c1edd7226fb6a0deec3c89c355d5c3e97b743869
Muslima <msamatovass@gmail.com>
2023-06-11 09:41:38 +0500

    new_restaurant

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Section from './components/Section';
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
-      <Section />
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logoimg from './img/LOGO.png'
-import issiqtaom from './img/Mask Group (1).png'
+import issiqtaom from './img/Mask Group (1).png';
+// import { Link } from 'react-router-dom';
 
 function Navbar() {
     return(
@@ -14,15 +15,16 @@ function Navbar() {
                         <div className="katalog d-md-none mt-3">
                             <a href="#"><i class=" fa-solid fa-bars"></i></a>
                         </div>
-                        <ul className="d-md-flex list d-sm-none mt-3">
+                        <ul className="d-md-flex list d-none mt-3">
                             <li className="text-light"><a>ГЛАВНАЯ</a></li>
                             <li className="text-light"><a>МЕНЮ</a></li>
                             <li className="text-light"><a>O HAC</a></li>
                             <li className="text-light"><a>БРОНЬ</a></li>
                         </ul>
-                        <ul className="d-lg-flex d-sm-none">
+                        <ul className="d-lg-flex d-none">
                             <li className="text-secondary p-3 border-end shop "><i class="fa-solid fa-cart-shopping"></i></li>
-                            <li className="text-light px-4"><i class="fa-solid fa-phone-volume"></i> +999-888-76-54 <br/><p className="navp">Свяжитесь c нами для <br/> бронирования</p></li>
+                            <li className="text-light px-4"><i class="fa-solid fa-phone-volume"></i> +999-888-76-54 <br/>
+                            <p className="navp">Свяжитесь c нами для <br/> бронирования</p></li>
                             <li><button className="btn1 text-light">3AKA3 СТОЛИКА</button></li>
                         </ul>
                     </div>
@@ -37,8 +39,7 @@ function Navbar() {
                         <button className="btn1">VIEW MENU</button>
                     </div>
                 </div>
-
-                <div className="divcard container row d-flex flex-row flex-wrap">
+                <div className="divcard container row">
                     <div className="card1 bg-light col-xxl-3 col-sm-12 col-md-12 col-lg-3 col-xl-3 text-center">
                         <img src={issiqtaom}/>
                         <h6 className="titleh6">Магическая <span className="textcolor">Атмосфера</span></h6>
